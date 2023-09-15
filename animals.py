0a1dd630c05c0ca5894d39ffa4135c0694313a27
Simon <57177898+Hellestyle@users.noreply.github.com>
2023-09-15 11:16:30 +0200

    Delete animals.py

--- animals.py
+++
@@ -1,5 +0,0 @@
-import random
-import sys
-animals = sys.stdin.read().splitlines(0)
-print(f'Hello Simon! Welcome to the terminal 🌮 ')
-print(f"Your random animal for this session is: {animals[random.randint(0,len(animals)-1)]}")
